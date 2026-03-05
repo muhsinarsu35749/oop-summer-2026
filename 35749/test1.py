2c72f829e24834f70813e1f7ff01d29a17e52e58
muhsinarsu35749 <muhsinarsu27@gmail.com>
2026-03-05 09:32:36 +0000

    test1 prep

--- 35749/test1.py
+++ 35749/test1.py
@@ -1,1 +1,11 @@
+#I am writing my first comment and this is so exciting
 
+name = input("Please enter your name here:")
+
+print(f"hello {name}")
+
+if name == ("muhsin"):
+    print("What a surprise we have the same name!")
+
+else:
+    print("Nice to meet you I am muhsin")
