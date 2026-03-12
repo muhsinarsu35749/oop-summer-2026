d30d9375768c237c94e891f96ec4fbe9cae5567c
muhsinarsu35749 <muhsinarsu27@gmail.com>
2026-03-12 10:32:49 +0000

    update

---
+++ Students/35749/lecture_2_entry_test.py
@@ -0,0 +1,15 @@
+my_birthday = 1
+print(my_birthday)
+print(type(my_birthday))
+
+my_name = "muhsin arsu"
+print(my_name)
+print(type(my_name))
+
+my_grade = 4.5
+print(my_grade)
+print(type(my_grade))
+
+converted_grade = int(my_grade)
+print(converted_grade)
+print(type(converted_grade))
