fbd0b71ad80e6f759e64166c8447a4866d769a14
muhsinarsu35749 <muhsinarsu27@gmail.com>
2026-03-12 10:10:37 +0000

    Lecture_2_entry_test.py

--- Students/35749/Lecture_2_entry_test.py
+++ Students/35749/Lecture_2_entry_test.py
@@ -2,14 +2,14 @@ my_birthday = 1
 print(my_birthday)
 print(type(my_birthday))
 
-my_name = muhsin_arsu
+my_name = "muhsin arsu"
 print(my_name)
 print(type(my_name))
 
 my_grade = 4.5
-print(my_name)
-print(type(my_name))
+print(my_grade)
+print(type(my_grade))
 
-converted_grade = print(int(my_name))
+converted_grade = int(my_grade)
 print(converted_grade)
 print(type(converted_grade))
